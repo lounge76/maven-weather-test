f303c28a180f96a4f465fb5dfd07b10c863d5583
lounge76 <Kilen123!>
2020-05-12 11:34:13 +0200

    fgfd

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -3,6 +3,8 @@ import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class main {
 
     public static void main(String args[])
@@ -12,6 +14,9 @@ public class main {
         WebDriver x = new FirefoxDriver();
 
         x.get("https://lounge76-weather-app.glitch.me/");
+
+        //assertEquals method Parameters: Expected Value, Actual Value, Assertion Message
+        assertEquals("Software Test Academy", driver.getTitle(), "Title check failed!");
 
     }
 }
