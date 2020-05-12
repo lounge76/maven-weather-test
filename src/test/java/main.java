6f3b7bdbd11de0d69985e3e8412146727bf4e6ac
lounge76 <Kilen123!>
2020-05-12 11:05:14 +0200

    Initial commit

---
+++ src/test/java/main.java
@@ -0,0 +1,17 @@
+import io.github.bonigarcia.wdm.DriverManagerType;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class main {
+
+    public static void main(String args[])
+    {
+        // saves us from system set property gecko...
+        FirefoxDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+        WebDriver x = new FirefoxDriver();
+
+        x.get("https://lounge76-weather-app.glitch.me/");
+        
+    }
+}
